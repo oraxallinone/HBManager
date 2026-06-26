d9b4193d4aff1b8afb096a7dcaa268250bbbb0ec
oraxallinone <oraxallinone@gmail.com>
2026-06-26 23:45:07 +0530

    update to 2026 support

--- HBManager/Areas/GstBill/Models/Model1.Designer.cs
+++ HBManager/Areas/GstBill/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\sync with git hub\After 2025 Oct 24\HBManager\HBManager\HBManager\Areas\GstBill\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\sync with git hub\SyncGit\HBManager\HBManager\Areas\GstBill\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
